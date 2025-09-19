ca9d48b162e2bf6457ada1e05e4823b181b35046
ayush <ayush9569patel@gmail.com>
2025-09-19 09:50:11 +0530

    fix the error in production

--- backend/index.js
+++ backend/index.js
@@ -28,10 +28,9 @@ const __dirname = path.resolve()
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-
-  })
+   app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 
 }
 
